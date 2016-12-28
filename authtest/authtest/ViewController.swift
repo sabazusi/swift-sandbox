4b9a8c0076331af43fb07531adae229e916482c3
sabazusi <kouhei.live@gmail.com>
2016-12-29 03:23:52 +0900

    Recreate new swift project

---
+++ authtest/authtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  authtest
+//
+//  Created by sabazusi on 2016/12/29.
+//  Copyright © 2016年 sabazusi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
