09aaf7e8f2ff382e85b294eb9248d66b262d30ff
sabazusi <kohei_tsurubo@dwango.co.jp>
2016-12-30 18:08:25 +0900

    Add ImageBoard project

---
+++ ImageBoard/ImageBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageBoard
+//
+//  Created by 鶴房　康平 on 2016/12/30.
+//  Copyright © 2016年 sabazusi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
