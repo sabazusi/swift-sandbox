fa27998b32518596f111a20198347c3502d1311e
sabazusi <kohei_tsurubo@dwango.co.jp>
2017-01-03 04:28:56 +0900

    Add collection view

--- ImageBoard/ImageBoard/ViewController.swift
+++ ImageBoard/ImageBoard/ViewController.swift
@@ -1,10 +1,28 @@
 import UIKit
 import SVProgressHUD
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     var timer: Timer!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        
+        let testCell:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        testCell.backgroundColor = UIColor.cyan
+        return testCell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellSize:CGFloat = self.view.frame.size.width/3-9
+        return CGSize(width: cellSize, height: cellSize)
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20;
     }
     
     override func viewDidAppear(_ animated: Bool) {
