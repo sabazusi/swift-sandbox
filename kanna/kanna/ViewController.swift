30fc0f92deedc0b344946646d1c0a5f2f4d6cf92
sabazusi <kouhei.live@gmail.com>
2017-09-23 20:19:27 +0900

    Add kanna project

---
+++ kanna/kanna/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kanna
+//
+//  Created by sabazusi on 9/23/17.
+//  Copyright © 2017 sabazusi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
